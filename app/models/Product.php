7130e9cbd2cf43de772503595b49cba7e84bf3c8
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-07-23 16:15:05 +0100

    product.php model modified

--- app/models/Product.php
+++ app/models/Product.php
@@ -1,15 +1,25 @@
 <?php
 
-class Category extends \Eloquent {
+class Product extends \Eloquent {
 
-    //to protect ourselves from mass assignment
-    protected $fillable = array('name');
+    //property specifies which attributes should be mass-assignable.
+    protected $fillable = array('category_id', 'title', 'description', 'price', 'availability', 'image');
 
-    public static $rules = array('name'=>'required|min:3');
+    //setting up validation rules
+    public static $rules = array(
+        'category_id'=>'required|integer',
+        'title'=>'required|min:2',
+        'description'=>'required|min:20',
+        'price'=>'required|numeric',
+        'availability'=>'integer',
+        'image'=>'required|image|mimes:jpeg,jpg,bmp,png,gif'
+    );
 
-    //creating relationship to specify that a category has many products
-    public function products() {
-        return $this->hasMany('Product');
+
+    //creating relationship to specify that each product belongs to one category
+    public function category() {
+        return $this->belongsTo('Category');
     }
+
 
 }
